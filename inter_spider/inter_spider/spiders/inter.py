40fadafdce7aba84401c246dfd52003d5ee60ac9
Yago Santos <yagossc@gmail.com>
2020-03-09 14:25:28 -0300

    Crawl initial page for the championship links

--- inter_spider/inter_spider/spiders/inter.py
+++ inter_spider/inter_spider/spiders/inter.py
@@ -9,8 +9,17 @@ class InterMatches(scrapy.Spider):
             'http://www.internacional.com.br/conteudo?modulo=3&setor=62'
         ]
 
+        # make a request for each url
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse_championship_links)
 
-    def parse(self, response):
-        print(response)
+    def parse_championship_links(self, response):
+        championships_xpath = '/html/body/section/div/div[1]/div/p'
+
+        championship_links = []
+
+        for p in response.xpath(championships_xpath):
+            next_link = p.xpath('./a/@href').get()
+            championship_links.append(response.urljoin(next_link))
+
+        print(championship_links)
